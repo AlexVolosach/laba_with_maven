4d8e4d2f2a8c585d2aba61183ffd5a873204b847
AlexVolosach <bigguy13052004@gmail.com>
2022-03-28 15:38:36 +0200

    new commit

--- src/main/java/com/github/stokito/unitTestExample/calculator/Calculator.java
+++ src/main/java/com/github/stokito/unitTestExample/calculator/Calculator.java
@@ -3,19 +3,19 @@ package com.github.stokito.unitTestExample.calculator;
 public class Calculator {
 
 	public int sum(int a, int b) {
-		return a + b;
+		return a + b;//ps
 	}
 
 	public int minus(int a, int b) {
-		return a + b;  // ERROR!!!
+		return a + b;  // ERROR!!!//ps
 	}
 //ps
 	public int divide(int a, int b) {
-		return a / b;
+		return a / b;//ps
 	}
 
 	public int multiply(int a, int b) {
-		return a * b;
+		return a * b;//ps
 	}
 
 }
